c79e4c7ded48531226a34e00d3d220f5da92beed
sridhar02 <kattasridhar02@gmail.com>
2023-10-25 22:16:52 +0530

    app init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl  bg-violet-600 p-4 text-white">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
