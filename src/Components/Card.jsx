40e9cd76153cb32d8b3bf2fb320ee801672d085c
sridhar02 <kattasridhar02@gmail.com>
2023-10-26 21:05:35 +0530

    card dialog open

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
+import { Dialog } from "./Dialog";
 
 export const Card = ({ item, index, parent }) => {
   let [isOpen, setIsOpen] = useState(false);
   const { title, id } = item;
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  const openDialog = () => setDialogOpen(true);
+  const closeDialog = () => setDialogOpen(false);
 
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: id,
@@ -27,6 +32,13 @@ export const Card = ({ item, index, parent }) => {
         <div className="" onClick={() => setIsOpen(!isOpen)}>
           {title}
         </div>
+        <Dialog
+          isOpen={isDialogOpen}
+          onClose={closeDialog}
+          title="Create a Project"
+        >
+          <h1>Task preview</h1>
+        </Dialog>
       </div>
     </>
   );
